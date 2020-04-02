85235b118b877f60ff62ebb6928e7096a76ff7f1
lizz <elizabeth.ann.soup@gmail.com>
2020-04-02 14:31:50 -0400

    Added solution

--- js/app.js
+++ js/app.js
@@ -1,59 +1,58 @@
-(function(){
-    const customerImage = document.querySelector('#customer-img')
-    const customerName = document.querySelector('#customer-name')
-    const customerText = document.querySelector('#customer-text')
-    const buttons = document.querySelectorAll('.btn')
-    let index = 0
-    const customers = []
+const img = document.getElementById('customer-img');
+const name = document.getElementById('customer-name');
+const review = document.getElementById('customer-text');
+const btn = document.querySelectorAll('.btn');
+const prevBtn = document.querySelector('.prevBtn');
+const nextBtn = document.querySelector('.nextBtn');
+const customers = [
+    {
+        img: './img/customer-0.jpg',
+        name: 'John', 
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis neque reprehenderit laborum, corporis explicabo assumenda. Porro impedit consectetur animi, reprehenderit recusandae sapiente at aliquam reiciendis modi ipsam rerum suscipit distinctio?'
+    },
+    {
+        img: './img/customer-1.jpg',
+        name: 'Sandy', 
+        review: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock'
+    },
+    {
+        img: './img/customer-2.jpg',
+        name: 'Amy', 
+        review: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.'
+    },
+    {
+        img: './img/customer-3.jpg',
+        name: 'Tyrell', 
+        review: 'If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text.'
+    },
+    {
+        img: './img/customer-4.jpg',
+        name: 'Wanda', 
+        review: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'}    
+];
 
-    //Create a new customer using a customer constructor
-    
-    //Customer Constructor
-    function Customer(img, name, text) {
-        this.img = img
-        this.name = name
-        this.text = text
-    }
+let counter = 0;
 
-    //Create new customer using the constructor function
+prevBtn.addEventListener('click', prev);
+nextBtn.addEventListener('click', next);
 
-    function createCustomer(img, name, text) {
+// Functions
+function prev() {
+    if (counter === 0) {
+        counter = customers.length;
+    };
+    counter--;
+    img.src = customers[counter]['img'];
+    name.innerHTML = customers[counter]['name'];
+    review.innerHTML = customers[counter]['review'];
+};
 
-        let fullImg = `./img/customer-${img}.jpg`
-        let customer = new Customer(fullImg, name, text)
-
-        customers.push(customer)
-    }
-
-    
-    createCustomer(0, 'John', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis neque reprehenderit laborum, corporis explicabo assumenda. Porro impedit consectetur animi, reprehenderit recusandae sapiente at aliquam reiciendis modi ipsam rerum suscipit distinctio?')
-    createCustomer(1, 'Sandy', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock')
-    createCustomer(2, 'Amy', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.')
-    createCustomer(3, 'Tyrell', 'If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text.')
-    createCustomer(4, 'Wanda', 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.')
-    
-
-    buttons.forEach(function(button){
-        button.addEventListener('click', function(e){
-            if (e.target.parentElement.classList.contains('prevBtn')){
-               if(index === 0){
-                    index = customers.length
-               }
-               index--
-               customerImage.src = customers[index].img
-               customerName.textContent = customers[index].name
-               customerText.textContent = customers[index].text
-            }
-            if (e.target.parentElement.classList.contains('nextBtn')){
-                index++
-                if(index === customers.length){
-                     index = 0
-                }
-                customerImage.src = customers[index].img
-                customerName.textContent = customers[index].name
-                customerText.textContent = customers[index].text
-             }
-        })
-    })
-    
-})()
+function next() {
+    counter++;
+    if (counter === customers.length) {
+        counter = 0;
+    };
+    img.src = customers[counter]['img'];
+    name.innerHTML = customers[counter]['name'];
+    review.innerHTML = customers[counter]['review']; 
+};
